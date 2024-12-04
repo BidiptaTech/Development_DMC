2209404bb1b3c07f3cd4ef22ffc657569fc40b7a
BidiptaTech <bidipta.mitra@coactivesolutions.co.in>
2024-12-04 15:48:46 +0530

    update rates of room

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -5,11 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use App\Models\Rate;
 class Room extends Model
 {
     use HasFactory;
     use SoftDeletes;
     protected $table = 'rooms'; 
     protected $guarded = []; 
+
+    public function rates()
+    {
+        return $this->belongsTo(Rate::class, 'rate_id');    
+    }
 }
