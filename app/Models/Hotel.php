940c91b6f2ba070dcfed9500c321ed2ae9478abf
BidiptaTech <bidipta.mitra@coactivesolutions.co.in>
2024-12-12 13:26:45 +0530

    hotels api modify

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -14,7 +14,13 @@ class Hotel extends Model
     protected $guarded = []; 
 
     public function rooms()
-{
-    return $this->hasMany(Room::class, 'hotel_id'); // Assuming `hotel_id` is the foreign key in the `rooms` table
-}
+    {
+        return $this->hasMany(Room::class, 'hotel_id'); // Assuming `hotel_id` is the foreign key in the `rooms` table
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'cat_id', 'category_id');
+    }
+
 }
