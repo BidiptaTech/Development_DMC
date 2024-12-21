b7c96af526b96c6b1c233fe7f97b09b5d4f8d5e1
BidiptaTech <bidipta.mitra@coactivesolutions.co.in>
2024-12-21 13:24:19 +0530

    ta view for hotels createy

--- resources/views/hotel/add-hotel.blade.php
+++ resources/views/hotel/add-hotel.blade.php
@@ -27,47 +27,45 @@
         <div class="row justify-content-center">
             <div class="col-lg-12 col-md-10 col-sm-12">
                 <div class="card">
+                <ul class="nav nav-pills mb-4 mt-4 d-flex justify-content-center" id="pills-tab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active" id="pills-hotel-tab" data-bs-toggle="pill" href="{{ route('hotels.create') }}" 
+                            role="tab" aria-controls="pills-hotel" aria-selected="true">
+                            Hotel Details
+                        </a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link disabled" id="pills-contact-tab" href="#" 
+                            role="tab" aria-controls="pills-contact" aria-selected="false" tabindex="-1">
+                            Contact Details
+                        </a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link disabled" id="pills-room-tab" href="#" 
+                            role="tab" aria-controls="pills-room" aria-selected="false" tabindex="-1">
+                            Room Details
+                        </a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link disabled" id="pills-season-tab" href="#" 
+                            role="tab" aria-controls="pills-season" aria-selected="false" tabindex="-1">
+                            Season Details
+                        </a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link disabled" id="pills-event-tab" href="#" 
+                            role="tab" aria-controls="pills-event" aria-selected="false" tabindex="-1">
+                            Event Details
+                        </a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link disabled" id="pills-calendar-tab" href="#" 
+                            role="tab" aria-controls="pills-calendar" aria-selected="false" tabindex="-1">
+                            Calendar
+                        </a>
+                    </li>
+                </ul>
 
-
-                    <ul class="nav nav-pills mb-4 mt-4 d-flex justify-content-center" id="pills-tab" role="tablist">
-                        <li class="nav-item" role="presentation">
-                            <a class="nav-link active" id="pills-hotel-tab" data-bs-toggle="pill" href="{{ route('hotels.create') }}" 
-                                role="tab" aria-controls="pills-hotel" aria-selected="true">
-                                Hotel Details
-                            </a>
-
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <a class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" href="{{ route('hotels.create') }}" 
-                                role="tab" aria-controls="pills-contact" aria-selected="false">
-                                Contact Details
-                            </a>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <a class="nav-link" id="pills-room-tab" data-bs-toggle="pill" href="#pills-room" 
-                                role="tab" aria-controls="pills-room" aria-selected="false">
-                                Room Details
-                            </a>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <a class="nav-link" id="pills-season-tab" data-bs-toggle="pill" href="#pills-season" 
-                                role="tab" aria-controls="pills-season" aria-selected="false">
-                                Season Details
-                            </a>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <a class="nav-link" id="pills-event-tab" data-bs-toggle="pill" href="#pills-event" 
-                                role="tab" aria-controls="pills-event" aria-selected="false">
-                                Event Details
-                            </a>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                            <a class="nav-link" id="pills-calendar-tab" data-bs-toggle="pill" href="#pills-calendar" 
-                                role="tab" aria-controls="pills-calendar" aria-selected="false">
-                                Calendar
-                            </a>
-                        </li>
-                    </ul>
 
 
                     <div class="card-header px-4 py-3" style="background-color: #e0bbf7; color: white;">
